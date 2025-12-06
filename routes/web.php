18e329b7e1265468ecb58ce214ce93cafbd62817
Alex <alexgmark@gmail.com>
2025-12-06 22:38:31 +0000

    Adding rate limiter

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('scan', [ScanController::class, 'create'])->name('scan.create');
-Route::post('scan', [ScanController::class, 'store'])->name('scan.store');
+Route::post('scan', [ScanController::class, 'store'])->middleware('throttle:scans')->name('scan.store');
 Route::get('scan/{scan}', [ScanController::class, 'show'])->name('scan.show');
 
 require __DIR__.'/settings.php';
